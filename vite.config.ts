2c6662d9dbc25e7026e81c9bba4931dbdb4b41fe
Isiaka Lukman <isiakalukmandellyson@gmail.com>
2025-06-16 08:50:52 +0100

    chore: Update package.json and vite.config.ts to use new file naming conventions for build outputs

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     lib: {
       entry: "src/main.ts",
       name: "WhatsAppWidget",
-      fileName: (format) => `kk-whatsapp-widget.${format}.js`,
+      fileName: (format) => `index.${format}.js`,
       formats: ["es", "umd", "iife"],
     },
     rollupOptions: {
